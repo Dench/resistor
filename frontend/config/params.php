ab01ed36763f938f010e0045e86cb63ffce3f5ef
Dench <dench@i.ua>
2016-03-20 23:14:03 +0200

    Обновление

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -18,4 +18,7 @@ return [
     'image_none' => [
         'small' => '/img/none_small.jpg',
     ],
+    'phone1' => '(093) 984-46-59',
+    'phone2' => '(093) 984-46-59',
+    'email' => 'info@cyprus.com',
 ];
