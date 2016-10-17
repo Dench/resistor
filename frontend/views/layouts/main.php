d78c18a9f279b71cb004cf8068e9bb96867a4fee
Alexey Dench <dench@i.ua>
2016-10-17 11:43:40 +0300

    Analytics

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -131,6 +131,15 @@ yii\bootstrap\Modal::begin([
 <div id='modal-send-content'></div>
 <?php yii\bootstrap\Modal::end(); ?>
 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-85595142-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
 <?php $this->endBody() ?>
 </body>
 </html>
